70d87c7d41ec1c93061134c4d3b56136f0baceeb
bianca9901 <biancawikberg99@yahoo.com>
2023-06-26 14:37:25 +0200

    Add hangman figures file

--- hangman_stages.py
+++ hangman_stages.py
@@ -1,76 +1,72 @@
 stages = [
-        """
-           --------
-           |      |
-           |      O
-           |     \|/
-           |      |
-           |     ] [
-        """,
-        """
-        
-        YOU HAVE 1 LIFE LEFT
-        
-           --------
-           |      |
-           |      O
-           |     \|/
-           |      |
-           |     ] 
-        """,
-        """
-        
-        YOU HAVE 2 LIVES LEFT
-        
-           --------
-           |      |
-           |      O
-           |     \|/
-           |      |
-           |      
-        """,
-        """
-        
-        YOU HAVE 3 LIVES LEFT
-        
-           --------
-           |      |
-           |      O
-           |     \|
-           |      |
-           |     
-        """,
-        """
-        
-        YOU HAVE 4 LIVES LEFT
-        
-           --------
-           |      |
-           |      O
-           |      |
-           |      |
-           |     
-        """,
-        """
-        
-        YOU HAVE 5 LIVES LEFT
-        
-           --------
-           |      |
-           |      O
-           |    
-           |      
-           |     
-        """,
-        """
-        
-        YOU HAVE 6 LIVES LEFT
-        
-           --------
-           |      |
-           |      
-           |    
-           |      
-           |     
-        """
-    ]
+    """
+    YOU HAVE 6 LIVES
+    
+       --------
+       |      |
+       |      
+       |    
+       |      
+       |     
+    """,
+    """
+    YOU HAVE 5 LIVES LEFT
+    
+       --------
+       |      |
+       |      O
+       |    
+       |      
+       |     
+    """,
+    """
+    YOU HAVE 4 LIVES LEFT
+    
+       --------
+       |      |
+       |      O
+       |      |
+       |      
+       |     
+    """,
+    """
+    YOU HAVE 3 LIVES LEFT
+    
+       --------
+       |      |
+       |      O
+       |     \|
+       |      
+       |     
+    """,
+    """
+    YOU HAVE 2 LIVES LEFT
+    
+       --------
+       |      |
+       |      O
+       |     \|/
+       |      
+       |     
+    """,
+    """
+    YOU HAVE 1 LIFE LEFT
+    
+       --------
+       |      |
+       |      O
+       |     \|/
+       |      |
+       |     ] 
+    """,
+    """
+    YOU HAVE 0 LIVES LEFT
+    
+       --------
+       |      |
+       |      O
+       |     \|/
+       |      |
+       |     ] [
+    """
+]
